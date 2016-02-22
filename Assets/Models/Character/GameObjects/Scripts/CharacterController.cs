626f7315e623a45d649148b8b59db514338e9869
WilliamBurdett <william.burdett1@gamil.com>
2016-02-22 03:22:13 -0800

    Controls almost work
    Next is targeting reticule and vertical mouse control.

---
+++ Assets/Models/Character/GameObjects/Scripts/CharacterController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+    private float horizontalBaseTurnSpeed =50000f;
+    private float verticalBaseTurnSpeed = 5000f;
+    private float movementSpeed = 20f;
+
+    // Use this for initialization
+    void Start () {
+        Cursor.lockState=CursorLockMode.Locked;
+        Cursor.visible = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        localMovementControl();
+    }
+
+    void localMovementControl() {
+        transform.Translate(Vector3.forward*(InputManager.getForward()+(-1*InputManager.getBackwards()))*Time.deltaTime*movementSpeed);
+        transform.Translate(Vector3.right*(InputManager.getStrafeRight()+(-1*InputManager.getStrafeLeft()))*Time.deltaTime*movementSpeed);
+        float horizontalSpeed = InputManager.getHorizontalTurn() * horizontalBaseTurnSpeed * Time.deltaTime;
+        float verticalSpeed = InputManager.getVerticalTurn() * verticalBaseTurnSpeed * Time.deltaTime;
+        transform.Rotate(new Vector3(0F,horizontalSpeed,0f)*Time.deltaTime);
+    }
+}
---
+++ Assets/Models/Character/GameObjects/Scripts/InputManager.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+    public static float getForward() {
+        if(Input.GetKey(KeyCode.W)) {
+            return 1f;
+        }
+        return 0;
+        //Controller support
+    }
+
+    public static float getHorizontalTurn() {
+        return Input.GetAxis("Mouse X");
+    }
+
+    public static float getVerticalTurn() {
+        return Input.GetAxis("Mouse Y");
+    }
+
+    public static float getBackwards() {
+        if(Input.GetKey(KeyCode.S)) {
+            return 1f;
+        }
+        return 0;
+        //Controller support
+    }
+
+    public static float getStrafeLeft() {
+        if(Input.GetKey(KeyCode.A)) {
+            return 1f;
+        }
+        return 0f;
+        //Controller support
+    }
+
+    public static float getStrafeRight() {
+        if(Input.GetKey(KeyCode.D)) {
+            return 1f;
+        }
+        return 0f;
+        //Controller support
+    }
+
+    public static bool getMainFire() {
+        if(Input.GetKeyDown(KeyCode.Mouse0)) {
+            return true;
+        }
+        //Controller right trigger
+        return false;
+    }
+
+    public static bool getSecondaryFire() {
+        if(Input.GetKeyDown(KeyCode.Mouse1)) {
+            return true;
+        }
+        //Controller left
+        return false;
+    }
+
+    public static bool switchWeapons() {
+        if(Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Alpha2)) {
+            return true;
+        }
+        //Controller Y button
+        return false;
+    }
+
+    public static bool jump() {
+        if(Input.GetKey(KeyCode.Space)) {
+            return true;
+        }
+        //Controller A button
+        return false;
+    }
+
+    public static bool melee() {
+        if(Input.GetKey(KeyCode.E)) {
+            return true;
+        }
+        //Controller E button
+        return false;
+    }
+}
